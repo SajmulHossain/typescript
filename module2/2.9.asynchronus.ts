9926961a72967ce9a35fe7dd2020a3c3c9711ded
Sajmul <sajmul1427@gmail.com>
2025-05-04 17:46:12 +0600

    asynchornus typescrip

--- module2/2.9.asynchronus.ts
+++ module2/2.9.asynchronus.ts
@@ -1,5 +1,42 @@
 // *asynchronus typescript
 
 {
-    
+    const createPromise = (): Promise<string> => {
+        return new Promise<string>((resolve, reject) => {
+            const data: string = 'something'
+
+            if(data) {
+                resolve(data)
+            } else {
+                reject("failed to load data");
+            }
+        })
+    }
+
+
+    // ? calling createPromise function
+
+    const showData = async() => {
+        const data = await createPromise();
+        console.log(data);
+    }
+
+    // showData();
+
+
+    interface Todo {
+        userId: number,
+        id: number,
+        title: string,
+        completed: boolean
+    }
+
+
+    const getTodo = async() : Promise<Todo> => {
+       const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+       const data = await res.json();
+       return data;
+    }
+
+    getTodo();
 }
